2d3f936d0e791007c8f239230563002f01ee440e
kumaran.a <kumaran.a@xerago.com>
2024-07-03 12:43:26 +0530

    37B

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const mysql = require('mysql2');
 const path = require('path');
-
+const cors = require('cors');
 const app = express();
 const port = process.env.PORT || 16513;
 
